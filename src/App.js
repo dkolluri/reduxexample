9e4eb7ea4bba83b9674afefb0bcca832c80c5b1b
divya kolluri <divyakolluri@divyas-air.home>
2020-04-01 14:21:17 -0700

    implementing react with redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Provider } from 'react-redux';
+import Posts from './components/Posts';
+import PostForm from "./components/PostForm";
+import store from './store';
 function App() {
   return (
+      <Provider store={store}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="flex-container">
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div>
+            <h1 className="title">
+              React with Redux.
+            </h1>
+          </div>
+        </div>
       </header>
+        <PostForm></PostForm>
+        <hr/>
+      <Posts></Posts>
     </div>
+      </Provider>
   );
 }
 
